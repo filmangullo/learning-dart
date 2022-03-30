46fc5e7b54ea2901eba5c60ca290cddec3ff8bff
filmangullo <filmantapgullo@gmail.com>
2022-03-30 13:53:06 +0700

    upload

---
+++ bin/hello_world.dart
@@ -0,0 +1,8 @@
+// The main() function is the top-level function of the Dart.
+// It is the most important and vital function of the Dart programming language.
+// The execution of the programming starts with the main() function.
+// The main() function can be used only once in a program.
+
+void main() {
+  print('Hello World');
+}
